722e382194a96a1e43b9d442b02d36ff9bdb675e
Dennis Kim <kim.junkoo@gmail.com>
2011-01-17 19:58:03 -0600

    Create class for 2D slider for colormap

--- Source/ColorPicker.js
+++ Source/ColorPicker.js
@@ -20,19 +20,40 @@ provides: ColorPicker
 */
 
 /*…*/
+var Slider2d = new Class({
+	
+	Implements: [Options, Events],
+	
+	options: {
+		xRange: [0, 255],
+		yRange: [0, 255],
+		element: 'cp1_colorMap',
+		prefix: 'cp1_'	
+	},
+	
+	initialize: function(pointer, options) {
+		this.pointer = pointer;
+		this.setOptions(options);
+		
+		this.xVal = 0;
+		this.yVal = 0;
+		
+		this.element = document.id(this.options.element);
+	}
+});
 
 var ColorPicker = new Class({
 
 	Implements: [Options, Events],
 
 	options: {
-		'element': 'colorPicker',		// element to contain ColorPicker UI
-		'prefix': 'cp1_',			// prefix for HTML elements
-		'input': ['rgb','hex'],	// ['rgb','hex','hsv'] which inputs to show/accept
-		'initColor': '#ff0000',	// starting color
-		'imgPath': '../../Source/img/',		// where the ColorPicker images are, relative to page or absolute
-		'lockTip': 'Select to modify only this value.',	// title attribute for lock radio buttons
-		'applyDefaultStyles': true	// set to false when using your own CSS styles
+		element: 'colorPicker',		// element to contain ColorPicker UI
+		prefix: 'cp1_',			// prefix for HTML elements
+		input: ['rgb','hex'],	// ['rgb','hex','hsv'] which inputs to show/accept
+		initColor: '#ff0000',	// starting color
+		imgPath: '../Source/img/',		// where the ColorPicker images are, relative to page or absolute
+		lockTip: 'Select to modify only this value.',	// title attribute for lock radio buttons
+		applyDefaultStyles: true	// set to false when using your own CSS styles
 	},
 
 	initialize: function(options) {
@@ -59,14 +80,42 @@ var ColorPicker = new Class({
 		this.buildPickerHTML();
 		this.buildInputsHTML();
 		this.buildHTMLStyles();
+		
+		this.mapSlider = new Slider2d({
+			element: this.colorMap,
+			prefix: this.options.prefix,
+			pointer: this.pointer
+		});
 	},
 
 	buildPickerHTML: function() {
 		var prefix = this.options.prefix;
 		
 		this.colorMap = new Element('div', {
-			id: prefix + 'colorMap'
+			id: prefix + 'colorMap',
+			styles: {
+				margin: '0',
+				padding: '0',
+				width: '256px',
+				height: '256px',
+				border: '1px solid white'
+			}
 		}).inject(this.element);
+		
+		this.pointer = new Element('div', {
+			id: this.options.prefix + 'pointer',
+			styles: {
+				background: "transparent url('" + this.options.imgPath + "mappoint.png') no-repeat 0 0",
+				margin: '0',
+				padding: '0',
+				width: '15px',
+				height: '15px',
+				border: '0 none',
+				position: 'absolute',
+				top: '0',
+				left: '0'
+			}			
+		}).inject(this.colorMap);
 		
 		this.colorBar = new Element('div', {
 			id: prefix + 'colorBar'
@@ -111,7 +160,8 @@ var ColorPicker = new Class({
 			that.inputs[key]['label'] = new Element('label', {
 				id: prefix + key + 'Label',
 				'class': prefix + 'Label',
-				html: key
+				'for': prefix + key + 'Val',
+				html: '<abbr title="' + that.inputs[key]['name'] + '">' + key + '</abbr>'
 			});
 			that.inputs[key]['wrapper'] = new Element('p', {
 				id: prefix + key + 'InputWrapper',
@@ -129,8 +179,8 @@ var ColorPicker = new Class({
 			}
 			that.inputs[val]['lock'].inject(container);
 			that.inputs[val]['val'].inject(container);
-			that.inputs[val]['label'].inject(container).grab(that.inputs[val]['val']);
-			that.inputs[val]['wrapper'].inject(container).adopt(that.inputs[val]['lock'], that.inputs[val]['label']);
+			that.inputs[val]['label'].inject(container);
+			that.inputs[val]['wrapper'].inject(container).adopt(that.inputs[val]['lock'], that.inputs[val]['label'], that.inputs[val]['val']);
 		});
 	},
 	
@@ -140,21 +190,33 @@ var ColorPicker = new Class({
 		if (this.options.applyDefaultStyles) {
 			$$('.' + prefix + 'Lock').each(function(el) {
 				el.setStyles({
-					width: '1.5em'
+					margin: '0 5px 0 0',
+					padding: '0',
+					width: '10px'
 				});
 			});
 			$$('.' + prefix + 'Val').each(function(el) {
 				el.setStyles({
-					marginLeft: '.2em',
-					width: '3.5em'
+					margin: '0 0 0 5px',
+					padding: '0',
+					width: '30px'
 				});
 			});
 			$$('.' + prefix + 'Label').each(function(el) {
 				el.setStyles({
-					width: '7em',
-					textAlign: 'right'
+					margin: '0',
+					padding: '0',
+					fontFamily: 'monospace',
+					fontSize: '12px'
 				});
-			})
+			});
+			$$('.' + prefix + 'InputWrapper').each(function(el) {
+				el.setStyles({
+					margin: '0',
+					padding: '0',
+					width: '70px'
+				});
+			});
 			
 		}
 	}
